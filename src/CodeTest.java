b5b493e5a9f75ca64ec1cf282127f4a8a2d90f3c
suprajaroyal <suprajamagisetty@gmail.com>
2025-06-19 19:03:14 +0530

    Second Commit

--- src/CodeTest.java
+++ src/CodeTest.java
@@ -5,33 +5,33 @@ import java.util.stream.Collectors;
 
 public class CodeTest {
     public static void main(String[] args) {
-        List<Object> vendor1 = new ArrayList<>();
-        vendor1.add("Samsung Galaxy S23");
-        vendor1.add("Samsung");
-        vendor1.add(56000);
+        List<Object> product1 = new ArrayList<>();
+        product1.add("Samsung Galaxy S23");
+        product1.add("Samsung");
+        product1.add(56000);
 
-        List<Object> vendor2 = new ArrayList<>();
-        vendor2.add("Motorollo");
-        vendor2.add("Motorolo");
-        vendor2.add(23000);
+        List<Object> product2 = new ArrayList<>();
+        product2.add("Motorollo Edge 50 pro");
+        product2.add("Motorollo");
+        product2.add(23000);
 
-        List<Object> vendor3 = new ArrayList<>();
-        vendor3.add("Iphone 16 promax");
-        vendor3.add("Iphone");
-        vendor3.add(42000);
+        List<Object> product3 = new ArrayList<>();
+        product3.add("Iphone 16 promax");
+        product3.add("Iphone");
+        product3.add(42000);
 
-        List<Object> vendor4 = new ArrayList<>();
-        vendor4.add("OnePlus Nord");
-        vendor4.add("Oneplus");
-        vendor4.add(25000);
+        List<Object> product4 = new ArrayList<>();
+        product4.add("OnePlus Nord");
+        product4.add("Oneplus");
+        product4.add(25000);
 
-        List<List<Object>> addVendor = new ArrayList<>();
-        addVendor.add(vendor1);
-        addVendor.add(vendor2);
-        addVendor.add(vendor3);
-        addVendor.add(vendor4);
+        List<List<Object>> products = new ArrayList<>();
+        products.add(product1);
+        products.add(product2);
+        products.add(product3);
+        products.add(product4);
 
-        List<List<Object>> sortByPrice = addVendor.stream()
+        List<List<Object>> sortByPrice = products.stream()
                 .sorted(Comparator.comparing(l -> (Integer) ((List<Object>) l).get(2)).reversed())
                 .collect(Collectors.toList());
 
